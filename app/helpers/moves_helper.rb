70862733bbbe1d4896aa87a1903e204403a32e2b
amax3002 <alexandremguy@gmil.com>
2016-12-29 17:20:45 -0500

    added a better timer that is in hours, min, sec

--- app/helpers/moves_helper.rb
+++ app/helpers/moves_helper.rb
@@ -7,13 +7,35 @@ module MovesHelper
   def gametimer(game_being_timed)
     first_move = Move.where(:player_id => Player.where(:user_id => current_user.id, :game_id => game_being_timed.id)).first
     last_move = Move.where(:player_id => Player.where(:user_id => current_user.id, :game_id => game_being_timed.id)).last
-    distance_of_time_in_words first_move.created_at, last_move.updated_at
+
+    seconds_diff = (first_move.created_at - last_move.created_at).to_i.abs
+
+    hours = seconds_diff / 3600
+    seconds_diff -= hours * 3600
+
+    minutes = seconds_diff / 60
+    seconds_diff -= minutes * 60
+
+    seconds = seconds_diff
+
+    "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
   end
 
   def gametimer_leaderboard(game_being_timed, user_input)
     first_move = Move.where(:player_id => Player.where(:user_id => user_input.id, :game_id => game_being_timed.id)).first
     last_move = Move.where(:player_id => Player.where(:user_id => user_input.id, :game_id => game_being_timed.id)).last
-    distance_of_time_in_words first_move.created_at, last_move.updated_at
+
+    seconds_diff = (first_move.created_at - last_move.created_at).to_i.abs
+
+    hours = seconds_diff / 3600
+    seconds_diff -= hours * 3600
+
+    minutes = seconds_diff / 60
+    seconds_diff -= minutes * 60
+
+    seconds = seconds_diff
+
+    "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
   end
 
 end
