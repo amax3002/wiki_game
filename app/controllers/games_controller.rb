df3521035d08d6ee3585efb53fb8392bae2f9c5c
amax3002 <alexandremguy@gmil.com>
2017-01-09 17:06:28 -0500

    saving

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -30,7 +30,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.html { redirect_to @game }
         format.json { render :show, status: :created, location: @game }
         flash[:success] = "Successfully Created a Game!"
       else
